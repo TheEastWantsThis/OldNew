ae48ae0c1be0a2ff3ae28926194e1ea2398d930f
TheEastWantsThis <reworldtoday@gmail.com>
2025-06-18 14:59:15 +0300

    first commit

--- internal/taskService/orm.go
+++ internal/taskService/orm.go
@@ -1,7 +1,7 @@
 package taskservice
 
 type RequestBodyTask struct {
-	ID             int    `gorm:"primaryKey";autoIncrement` //json:"ID"
+	ID             int    `gorm:"primaryKey autoIncrement"` //json:"ID"
 	Task           string `json:"Task"`
 	Accomplishment bool   `json:"Accomplishment"`
 }
